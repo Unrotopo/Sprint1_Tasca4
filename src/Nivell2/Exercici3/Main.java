2cd6f09d601344a4c8cb90207652cc69b05d955f
Luis Portas <otrorotopo@gmail.com>
2025-03-20 16:06:09 +0100

    Exercici4, fet!

---
+++ src/Nivell2/Exercici3/Main.java
@@ -0,0 +1,6 @@
+package Nivell2.Exercici3;
+
+public class Main {
+    public static void main(String[] args) {//VOID}
+    }
+}
---
+++ src/Nivell2/Exercici4/entities/Onions.java
@@ -0,0 +1,14 @@
+package Nivell2.Exercici4.entities;
+
+public class Onions {
+
+    String colour;
+    String flavourProfile;
+    String shape;
+
+    public Onions(String colour, String flavourProfile, String shape) {
+        this.colour = colour;
+        this.flavourProfile = flavourProfile;
+        this.shape = shape;
+    }
+}
